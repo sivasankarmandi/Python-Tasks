b61ff807c3c5fcd75b9bbf03edd74ed558553be5
sivasankarmandi <138428098+sivasankarmandi@users.noreply.github.com>
2023-08-28 13:14:00 +0530

    Add files via upload

---
+++ max_min.py
@@ -0,0 +1,7 @@
+# 148. Write a Python function to find the maximum and minimum numbers from a sequence of numbers.
+# Note: Do not use built-in functions.
+
+# Code :- 
+x=[1,4,8,34,87,37,223,678]
+x.sort()
+print("min value is:-",x[0],  "max value is :-",x[-1])
---
+++ type_test.py
@@ -0,0 +1,5 @@
+# 145. Write a Python program to test if a variable is a list, tuple, or set.
+
+# Code:-
+x=[1,2,4,56,67]
+print(type(x))
