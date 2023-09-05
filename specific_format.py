3483e9aa7fa7038d2d4b7066f3bb099bb9700bbf
sivasankarmandi <138428098+sivasankarmandi@users.noreply.github.com>
2023-09-05 15:44:06 +0530

    Delete specific_format.py

--- specific_format.py
+++
@@ -1,1 +0,0 @@
-print("Twinkle,twinkle,little star,\n \t How I wonder what you are ! \n \t \t up above the world so high ,\n \t \t Like a diamond in the sky.\n twinkle twinkle littele star , \n \t How I wonder what you are !")
