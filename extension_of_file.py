ddbf41ad75d45787feea4d06d876b523c724461a
sivasankarmandi <138428098+sivasankarmandi@users.noreply.github.com>
2023-09-05 15:41:24 +0530

    Delete extension_of_file.py

--- extension_of_file.py
+++
@@ -1,9 +0,0 @@
-#Write a Python program that accepts a filename from the user and prints the extension of the file.
-#Sample filename : abc.java
-#Output : java
-
-
-#code :-
-import pathlib
-extension = pathlib.Path("siva.py").suffix
-print(extension)
