68f4b9698ad4fe24369c7cd229c6c56c916881cd
sivasankarmandi <138428098+sivasankarmandi@users.noreply.github.com>
2023-09-05 15:43:56 +0530

    Delete schedule_exam.py

--- schedule_exam.py
+++
@@ -1,7 +0,0 @@
-#Write a Python program to display the examination schedule. (extract the date from exam_st_date).
-#exam_st_date = (11, 12, 2014)
-#Sample Output : The examination will start from : 11 / 12 / 2014
-
-#code :-
-exam=(11,12,2014)
-print("The examination schedule will start form : %i,%i,%i"%exam)
