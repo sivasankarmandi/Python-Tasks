220965d7ede362dc6113a5990e873c005c136949
sivasankarmandi <138428098+sivasankarmandi@users.noreply.github.com>
2023-09-05 15:43:46 +0530

    Delete reverse_order.py

--- reverse_order.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
-
-#Code :-
-
-S1 = input("enter the first name :",)
-S2 = input ("enter the last name :,")
-S3 = S1 + S2
-print(S3)
-print(S3[::-1])
